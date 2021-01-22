4fc485cf6cee1db78281bbf0d26935a1b2c7244b
scottyzen <scottyzen@gmail.com>
2021-01-22 15:10:31 +0000

    Log out put of coverter

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,7 @@
 module.exports = function(grunt) {
 
+  let names = [];
+  
     // This will find every instance of:  data-name="$LAYER_NAME$"
     // and repace it with:                data-name="LAYER_NAME" style="fill: var(--LAYER_NAME)"    
     grunt.initConfig({
@@ -10,16 +12,27 @@ module.exports = function(grunt) {
               replacements: [{
                 from: /data-name\=\"\$(.*?)\$"/gi,
                 to: function (matchedWord, index, fullText, regexMatches) {
-                  const outputWithStyleVariable = `data-name="${regexMatches[0]}" style="fill: var(--${regexMatches[0]})"`;
+                  
+                  let word = regexMatches[0].toLowerCase();
+
+                  names.push(word)
+
+
+
+                  const outputWithStyleVariable = `data-name="${word}" style="fill: var(--${word})"`;
                   console.log(outputWithStyleVariable)
                   return outputWithStyleVariable;
                 }
               }],
               dest: 'new/'
             }
-          }
+          },
+          printClassNames: {}
     })
 
     grunt.loadNpmTasks('grunt-text-replace');
-    grunt.registerTask('default', ['replace']);
+    grunt.registerTask('printClassNames', () => {
+      console.table([...new Set(names)]); // Print out each layer name
+    });
+    grunt.registerTask('default', ['replace', 'printClassNames']);
 };
